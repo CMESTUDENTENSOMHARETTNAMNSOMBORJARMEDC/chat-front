e73c47c4ab957eea9a9e96c31066c10903686f89
rrirrirr <drpihl@gmail.com>
2022-08-25 17:38:47 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    import.meta.env.MODE !== 'production'
+    import.meta.env.NODE_ENV !== 'production'
       ? react({
           jsxRuntime: 'classic',
         })
