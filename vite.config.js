2b614fc513a3ce23d25867d910d56c3f1229731c
rrirrirr <drpihl@gmail.com>
2022-08-25 17:36:05 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [
-    process.env.MODE !== 'production' ? react({
-      jsxRuntime: 'classic',
-    }) : react(),
-    svgLoader(),
+  plugins: [
+    import.meta.env.MODE !== 'production'
+      ? react({
+          jsxRuntime: 'classic',
+        })
+      : react(),
   ],
 })
